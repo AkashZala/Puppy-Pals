9284dcd3b7a3dbe1fe06f9370d4ade95b75d41fa
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-05 16:45:11 -0400

    get selected puppy data and display it, imported css files back

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
+import './App.css'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((puppy)=> puppy.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
       <div>
         { 
           puppies.map( puppy => {
-            return <p key={ puppy.id }>{ puppy.name }</p>
+            return <p onClick={ () => {
+              setFeatPupId(puppy.id)
+            }
+            } key={ puppy.id }>{ puppy.name }</p>
           })
         }
+          { 
+          featPupId && (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
       </div>
     </>
   )
